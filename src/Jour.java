3b405b4e0672409925854010d5e9e4e403d95863
Ritice <88079918+Ritice@users.noreply.github.com>
2023-11-22 14:14:50 +0100

    second commit

--- src/Jour.java
+++ src/Jour.java
@@ -1,5 +1,6 @@
 public class Jour {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Hello world! comment tu vas tu te porte bien?");
     }
 }
